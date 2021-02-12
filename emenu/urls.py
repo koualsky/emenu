1a63b1e23b6ba54fb61727b6be4624824694aef3
koualsky <koualsky@gmail.com>
2021-02-12 14:21:59 +0100

    Removed unnecessary imports

--- emenu/urls.py
+++ emenu/urls.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
 from django.urls import include, path
 
 urlpatterns = [
