2acd73d8a9f63b630943f14291d953ccfe43bfd5
koualsky <koualsky@gmail.com>
2021-02-18 20:55:54 +0100

    Changed draft of run_sending_emails command

--- scripts/run_sending_emails.sh
+++ scripts/run_sending_emails.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 cd path/to/your/django/project
-source venv/bin/activate  # if you use venv
-python manage.py closepoll  # maybe you want to >> /path/to/log so you can log the results
+source venv/bin/activate
+python manage.py closepoll
