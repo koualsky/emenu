1f83cc0fe36a93675fb902877c6913682502821f
koualsky <koualsky@gmail.com>
2021-02-18 19:17:58 +0100

    Added sending emails command

---
+++ scripts/run_sending_emails.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+cd path/to/your/django/project
+source venv/bin/activate  # if you use venv
+python manage.py closepoll  # maybe you want to >> /path/to/log so you can log the results
